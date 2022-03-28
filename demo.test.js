79930079b73515921b9161f934e1b79f78a5fa14
LeMonocle <84313315+LeMonocle@users.noreply.github.com>
2022-03-28 16:16:39 +0200

    Jest_test_maj

--- demo.test.js
+++
@@ -1,4 +0,0 @@
-test ("Demo somme", function(){
-    var a = 2 + 2;
-    expect(a).toBe(4);
-})
---
+++ tests/demo.test.js
@@ -0,0 +1,13 @@
+describe("Pack Demo", function(){
+
+    it("Demo somme 1", function(){
+        var a = 2 + 2;
+        expect(a).toBe(4);
+    })
+
+    it("Demo somme 2", function(){
+        var a = 2 + 1;
+        expect(a).toBe(3);
+    })
+})
+
