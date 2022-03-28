6a869dcd3e014da04bcfc832eb25523fedf9ca6d
LeMonocle <84313315+LeMonocle@users.noreply.github.com>
2022-03-28 17:53:05 +0200

    Récursivité et exercice

--- exemple.js
+++ exemple.js
@@ -57,3 +57,71 @@ console.log(addition2(premierNombre, deuxiemeNombre));
 let resultatAddition = addition2(premierNombre, deuxiemeNombre);
 let resultatDivision = division2(premierNombre, deuxiemeNombre);
 console.log(resultatAddition * resultatDivision);
+
+//PARAMETRES PAR DEFAUT
+let nombreTest = 4;
+
+
+function addition(nombreA, nombreB = 10) { //nombreB est un paramètre par défaut
+    let result = nombreA + nombreB;
+    return result;
+}
+
+console.log("Addition des nombres : " + addition(nombreTest));
+
+let preparation = 10, cuisson = 15;
+
+function cuisiner(nombreDeGateaux, minutesDePreparation = preparation, minutesDeCuisson = cuisson) {
+    let resultat = nombreDeGateaux * (minutesDeCuisson + minutesDePreparation);
+    return resultat;
+}
+
+let tempsDePreparationChocolat = cuisiner(4);
+let tempsDePreparationFraisier = cuisiner(1, 20); //On change un des paramètres par défaut
+
+console.log("Temps de préparation fraisier : " + tempsDePreparationFraisier);
+
+console.log("Temps de préparation total : " + tempsDePreparationChocolat + tempsDePreparationFraisier);
+
+// FONCTIONS ANONYMES (utiles pour les objets)
+
+let fonctionAnonyme = function(){
+    console.log("Je suis une fonction anonyme");
+}
+fonctionAnonyme();
+
+(function() {console.log("Je suis une autre fonction anonyme") })(); //Exécutée automatiquement, plus rapide !! Ne pas oublier les parenthèses, pas de ";", couple de parenthèses à la fin.
+
+//TEST SUR LA PORTEE DES VARIABLES
+
+let variableLet = "globale";
+var variableVar = "globale";
+
+if(true){
+
+    let variableLet = "locale";
+    var variableVar = "locale";
+
+    console.log("let L : " + variableLet);
+    console.log("var L : " + variableVar);
+
+}
+
+console.log("let G : " + variableLet);
+console.log("var G : " + variableVar);
+
+// BOUCLES RECURSIVES
+
+function timer(secondes){
+
+    if(secondes > 0){
+        console.log(secondes);
+        timer(secondes - 1);
+    }
+    else {
+        console.log(secondes);
+    }
+
+}
+
+timer(10);
