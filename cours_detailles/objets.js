e1bad8d74babf22596691137335afcd686737215
LeMonocle <84313315+LeMonocle@users.noreply.github.com>
2022-03-30 12:38:11 +0200

    Objets - MAJ

---
+++ cours_detailles/objets.js
@@ -0,0 +1,101 @@
+// LES OBJETS
+
+let chien = {
+    race: "shiba",
+    poil: "court",
+    // aboyer: function () {
+    //     console.log("Ouaf, ouaf !");
+    // } 
+    aboyer: () => console.log("Ouaf, ouaf")     //Pas de ';'
+}
+
+console.log(chien.race);
+chien.aboyer();
+
+let magicien = {
+    attaquer: function(){
+        console.log("Le magicien lance un sort.");
+    }
+}
+
+let guerrier = {
+    attaquer: function(){
+        console.log("Le guerrier tire son épée.");
+    }
+}
+
+magicien.attaquer();        //Fonction personnalisée à l'objet à laquelle elle est rattachée
+guerrier.attaquer();
+
+//L'AFFECTATION PAR DECOMPOSITION (DESTRUCTURING ASSIGNMENT)
+
+console.log("---------- AFFECTATION PAR DECOMPOPSITION ----------");
+
+let informations = ["SuperSayen", "25", "homme"];
+
+// let pseudo = informations[0];       // permet de ne pas corrompre les premières données
+// let age = informations[1];          // Methode sans décomposition... long !
+// let genre = informations[2];
+
+//Avec décomposition
+let [pseudo, age, genre] = informations;
+
+console.log(pseudo);
+console.log(age);
+console.log(genre);
+
+// L'OBJET SET
+
+console.log("---------- OBJET SET ----------");
+
+let nombres = [10, 45, 75, 10, 24, 45];
+let monSet = new Set(nombres);
+
+console.log(monSet);        //Les valeurs en double ont été supprimées
+
+let monSet2 = new Set();
+
+monSet2.add("70");
+monSet2.add(87);
+monSet2.add(88);
+monSet2.add(["titre", "auteur", "édition"]);
+
+console.log(monSet2);
+
+monSet2.delete(87);
+
+console.log(monSet2);
+console.log("J'ai " + monSet2.size + " éléments dans mon Set.");
+
+// OBJET MAP            RELIE CLEF/VALEUR
+
+console.log("---------- OBJET MAP ----------");
+
+let monMap = new Map([
+    ["prenom", "Mark"],
+    ["Nom", "Zuckerberg"]
+]);
+
+console.log(monMap);
+
+monMap.set("poste", "PDG de Facebook");         //AJOUTER
+console.log(monMap);
+
+monMap.delete("poste", "PDG de Facebook");      //SUPPRIMER
+console.log(monMap);
+
+    //sert pour des éléments complets !!!
+
+    let utilisateurs = new Map();
+
+    utilisateurs.set("Mark Zuckerberg",{
+        email: "mark@facebook.com",
+        poste: "PDG"
+    });
+
+    utilisateurs.set("Bill Gates", {
+        email: "bill@gatesnote.com",
+        poste: "Sauver le monde"
+    })
+
+    console.log(utilisateurs);
